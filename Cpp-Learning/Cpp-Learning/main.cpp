3bfb8e511df1051262d12e11fb66df023e52c634
fxzjshm <fxzjshm@163.com>
2016-12-25 13:05:01 +0800

    Import

---
+++ Cpp-Learning/Cpp-Learning/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "version.h"
+
+using namespace std;
+
+
+void hello_world()
+{
+    cout << "Hello world!" << endl;
+}
+
+void get_number_of_inputs()
+{
+    int a = 0;
+
+}
+
+int main()
+{
+    // hello_world();
+    return 0;
+}
---
+++ Cpp-Learning/Cpp-Learning/version.h
@@ -0,0 +1,24 @@
+#ifndef VERSION_H
+#define VERSION_H
+
+	//Software Status
+	static const char STATUS[] =  "Alpha";
+	static const char STATUS_SHORT[] =  "a";
+	
+	//Standard Version Type
+	static const long MAJOR  = 0;
+	static const long MINOR  = 0;
+	static const long BUILD  = 0;
+	static const long REVISION  = 0;
+	
+	//Miscellaneous Version Types
+	static const long BUILDS_COUNT  = 1;
+	#define RC_FILEVERSION 0,0,0,0
+	#define RC_FILEVERSION_STRING "0, 0, 0, 0\0"
+	static const char FULLVERSION_STRING [] = "0.0.0.0";
+	
+	//These values are to keep track of your versioning state, don't modify them.
+	static const long BUILD_HISTORY  = 0;
+	
+
+#endif //VERSION_H

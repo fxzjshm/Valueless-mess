ab1cb2c0f98393975a067cfa3b91bf96095416bb
fxzjshm <fxzjshm@163.com>
2021-02-20 00:32:37 +0800

    [montecarlo-pi] Add OpenCL adder (reduce method) but triggered a driver compiler bug.

--- rust-playground/montecarlo-pi/src/opencl_calculator.rs
+++ rust-playground/montecarlo-pi/src/opencl_calculator.rs
@@ -3,13 +3,15 @@ extern crate ocl;
 use ocl::ProQue;
 use crate::MonteCarloPiCalculator;
 use std::sync::Arc;
-use crate::multi_thread_calculator::MultiThreadCalculator;
-use self::ocl::core::DeviceInfo;
-use self::ocl::{Device, Platform};
-use self::ocl::builders::DeviceSpecifier;
+use self::ocl::core::{DeviceInfo, MEM_READ_ONLY, get_kernel_work_group_info, KernelWorkGroupInfo, MEM_READ_WRITE};
+use self::ocl::{Device, Platform, Buffer};
+use self::ocl::builders::{DeviceSpecifier, ProgramBuilder};
 use rayon::prelude::*;
-use std::rc::Rc;
 use std::time::Instant;
+use self::ocl::enums::KernelWorkGroupInfoResult;
+use core::mem;
+use self::ocl::core::ffi::clSetKernelArg;
+use std::ptr::null;
 
 pub struct OpenCLThreadCalculator {
     pro_que: ProQue,
@@ -45,6 +47,28 @@ static KERNEL_SRC: &'static str = r#"
         xs[i] = gen_random(t);
         ys[i] = gen_random(xs[i]);
     }
+
+    // reference: https://github.com/maoshouse/OpenCL-reduction-sum/blob/master/sum.cl
+    __kernel void cal(__global real* const xs, __global real* const ys, __global unsigned long long* output, __local unsigned long long* reductionSums) {
+        const int global_id = get_global_id(0),
+                     local_id = get_local_id(0),
+                     local_size = get_local_size(0),
+                     work_group_id = global_id / local_size;
+        real x = xs[global_id], y = ys[global_id];
+        unsigned long long is_inside = (sqrt(x * x + y * y) > (real) 1.0) ? 1 : 0;
+        reductionSums[local_id] = is_inside;
+
+        for (int offset = local_size / 2; offset > 0; offset /= 2) {
+		    barrier(CLK_LOCAL_MEM_FENCE);	// wait for all other work-items to finish previous iteration.
+		    if (local_id < offset) {
+		    	reductionSums[local_id] += reductionSums[local_id + offset];
+		    }
+        }
+
+        if (local_id == 0) {
+            output[work_group_id] = reductionSums[0];
+        }
+    }
 "#;
 
 impl OpenCLThreadCalculator {
@@ -66,10 +90,11 @@ impl OpenCLThreadCalculator {
         let kernel = self.pro_que.kernel_builder("gen_randoms").arg(&buffer_xs).arg(&buffer_ys).build()?;
         let start_time = Instant::now();
         unsafe { kernel.enq()?; }
-        self.pro_que.finish();
+        self.pro_que.finish()?;
         println!("kernel enqueue used: {}ms", start_time.elapsed().as_millis());
         buffer_xs.read(&mut xs).enq()?;
         buffer_ys.read(&mut ys).enq()?;
+        self.pro_que.finish()?;
         return ocl::Result::Ok((xs, ys));
     }
 
@@ -82,10 +107,11 @@ impl OpenCLThreadCalculator {
         let kernel = self.pro_que.kernel_builder("gen_randoms").arg(&buffer_xs).arg(&buffer_ys).build()?;
         let start_time = Instant::now();
         unsafe { kernel.enq()?; }
-        self.pro_que.finish();
+        self.pro_que.finish()?;
         println!("kernel enqueue used: {}ms", start_time.elapsed().as_millis());
         buffer_xs.read(&mut xs).enq()?;
         buffer_ys.read(&mut ys).enq()?;
+        self.pro_que.finish()?;
         let mut xs_ret = vec![0.0; n];
         xs_ret.par_iter_mut().enumerate().for_each(|(i, val)| {
             *val = xs[i] as f64;
@@ -95,6 +121,49 @@ impl OpenCLThreadCalculator {
             *val = ys[i] as f64;
         });
         return ocl::Result::Ok((xs_ret, ys_ret));
+    }
+
+    #[inline]
+    fn try_cal(&self, xs: &Arc<Vec<f64>>, ys: &Arc<Vec<f64>>, n: usize) -> ocl::Result<u64> {
+        if (self.use_f32) {
+            return self.try_cal_f32(xs, ys, n);
+        } else {
+            return self.try_cal_f64(xs, ys, n);
+        }
+    }
+
+    #[inline]
+    fn try_cal_f64(&self, xs: &Arc<Vec<f64>>, ys: &Arc<Vec<f64>>, n: usize) -> ocl::Result<u64> {
+        let buffer_xs = Buffer::<f64>::builder().queue(self.pro_que.queue().clone()).flags(MEM_READ_ONLY).len(n).copy_host_slice(&xs).build()?;
+        let buffer_ys = Buffer::<f64>::builder().queue(self.pro_que.queue().clone()).flags(MEM_READ_ONLY).len(n).copy_host_slice(&ys).build()?;
+        let buffer_dummy = Buffer::<u64>::builder().queue(self.pro_que.queue().clone()).len(1).build()?;
+        let kernel = self.pro_que.kernel_builder("cal").arg(&buffer_xs).arg(&buffer_ys).arg(&buffer_dummy).arg_local::<u64>(n).build()?;
+        let work_group_size;
+        if let KernelWorkGroupInfoResult::WorkGroupSize(wg_size) = get_kernel_work_group_info(&kernel, self.pro_que.device(), KernelWorkGroupInfo::WorkGroupSize).unwrap() {
+            work_group_size = wg_size;
+        } else {
+            unreachable!();
+        }
+        let buffer_partial_count = Buffer::<u64>::builder().queue(self.pro_que.queue().clone()).len(work_group_size).build()?;
+        kernel.set_arg(2, &buffer_partial_count)?;
+        unsafe { clSetKernelArg(kernel.as_ptr(), 3, mem::size_of::<u64>() * work_group_size, null()); }
+        let start_time = Instant::now();
+        unsafe { kernel.enq()?; }
+        self.pro_que.finish()?;
+        println!("kernel enqueue used: {}ms", start_time.elapsed().as_millis());
+        let mut partial_count = vec![0 as u64; work_group_size];
+        buffer_partial_count.read(&mut partial_count).enq()?;
+        self.pro_que.finish()?;
+        let mut ret = 0;
+        for x in partial_count {
+            ret += x;
+        }
+        return ocl::Result::Ok(ret);
+    }
+
+    #[inline]
+    fn try_cal_f32(&self, xs: &Arc<Vec<f64>>, ys: &Arc<Vec<f64>>, n: usize) -> ocl::Result<u64> {
+        unimplemented!();
     }
 }
 
@@ -128,6 +197,6 @@ impl MonteCarloPiCalculator for OpenCLThreadCalculator {
 
     #[inline]
     fn cal(&self, xs: &Arc<Vec<f64>>, ys: &Arc<Vec<f64>>, n: usize) -> u64 {
-        return MultiThreadCalculator::cal_static(xs, ys, n);
+        return self.try_cal(xs, ys, n).expect("Call to OpenCL kernel failed");
     }
 }

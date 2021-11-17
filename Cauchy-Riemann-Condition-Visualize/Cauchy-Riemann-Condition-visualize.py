0f38d4892d023a177ebe2dd2df76dd5ba5fb7313
fxzjshm <fxzjshm@163.com>
2021-11-18 00:25:22 +0800

    [CR-cond-visualize] add part of subtitles

--- Cauchy-Riemann-Condition-Visualize/Cauchy-Riemann-Condition-visualize.py
+++ Cauchy-Riemann-Condition-Visualize/Cauchy-Riemann-Condition-visualize.py
@@ -1,3 +1,4 @@
+from typing_extensions import runtime
 from manimlib import *
 
 # global parameters
@@ -13,6 +14,11 @@ base_point_x = 1.
 base_point_y = 2.
 dx = 0.2
 dy = 0.2
+
+dx_color = GREEN
+dy_color = RED
+u_v_dx_color = BLUE
+u_v_dy_color = ORANGE
 
 class CR_Visualize_left_side(Scene):
     def construct(self):
@@ -46,8 +52,8 @@ class CR_Visualize_left_side(Scene):
         # dx, dy
         dx_point = c_grid.coords_to_point(base_point_x + dx, base_point_y)
         dy_point = c_grid.coords_to_point(base_point_x, base_point_y + dy)
-        dx_dot = Dot(dx_point, color=GREEN)
-        dy_dot = Dot(dy_point, color=RED)
+        dx_dot = Dot(dx_point, color=dx_color)
+        dy_dot = Dot(dy_point, color=dy_color)
         
         self.play(
             Write(dx_dot),
@@ -96,16 +102,16 @@ class CR_Visualize_left_side(Scene):
         base_point = base_dot.get_center()
         dx_point = dx_dot.get_center()
         dy_point = dy_dot.get_center()
-        v_dx = Arrow(start=base_point, end=dx_point, buff=0, stroke_color=GREEN, stroke_width = arrow_stroke_width, max_tip_length_to_length_ratio=arrow_max_tip_length_to_length_ratio)
-        v_dy = Arrow(start=base_point, end=dy_point, buff=0, stroke_color=RED, stroke_width = arrow_stroke_width, max_tip_length_to_length_ratio=arrow_max_tip_length_to_length_ratio)
+        v_dx = Arrow(start=base_point, end=dx_point, buff=0, stroke_color=dx_color, stroke_width = arrow_stroke_width, max_tip_length_to_length_ratio=arrow_max_tip_length_to_length_ratio)
+        v_dy = Arrow(start=base_point, end=dy_point, buff=0, stroke_color=dy_color, stroke_width = arrow_stroke_width, max_tip_length_to_length_ratio=arrow_max_tip_length_to_length_ratio)
         v_dx_label = TexText("$\Delta x$")
         v_dx_label.scale(frame_scale * text_scale)
         v_dx_label.next_to(dx_point, DOWN*(frame_scale * text_scale))
-        v_dx_label.set_color(GREEN)
+        v_dx_label.set_color(dx_color)
         v_dy_label = TexText("$\Delta y$")
         v_dy_label.scale(frame_scale * text_scale)
         v_dy_label.next_to(dy_point, LEFT*(frame_scale * text_scale))
-        v_dy_label.set_color(RED)
+        v_dy_label.set_color(dy_color)
 
         self.play(
             Write(v_dx),
@@ -192,8 +198,8 @@ class CR_Visualize_right_side(Scene):
         # dx, dy
         dx_point = moving_c_grid.coords_to_point(base_point_x + dx, base_point_y)
         dy_point = moving_c_grid.coords_to_point(base_point_x, base_point_y + dy)
-        dx_dot = SmallDot(dx_point, color=GREEN)
-        dy_dot = SmallDot(dy_point, color=RED)
+        dx_dot = SmallDot(dx_point, color=dx_color)
+        dy_dot = SmallDot(dy_point, color=dy_color)
 
         self.play(
             Write(dx_dot),
@@ -248,16 +254,16 @@ class CR_Visualize_right_side(Scene):
         base_point = base_dot.get_center()
         dx_point = dx_dot.get_center()
         dy_point = dy_dot.get_center()
-        v_dx = Arrow(start=base_point, end=dx_point, buff=0, stroke_color=GREEN, stroke_width = arrow_stroke_width, max_tip_length_to_length_ratio=arrow_max_tip_length_to_length_ratio)
-        v_dy = Arrow(start=base_point, end=dy_point, buff=0, stroke_color=RED, stroke_width = arrow_stroke_width, max_tip_length_to_length_ratio=arrow_max_tip_length_to_length_ratio)
+        v_dx = Arrow(start=base_point, end=dx_point, buff=0, stroke_color=dx_color, stroke_width = arrow_stroke_width, max_tip_length_to_length_ratio=arrow_max_tip_length_to_length_ratio)
+        v_dy = Arrow(start=base_point, end=dy_point, buff=0, stroke_color=dy_color, stroke_width = arrow_stroke_width, max_tip_length_to_length_ratio=arrow_max_tip_length_to_length_ratio)
         v_dx_label = TexText("$\Delta_x f$")
         v_dx_label.scale(frame_scale * text_scale)
         v_dx_label.next_to(dx_dot.get_center(), RIGHT*(frame_scale * text_scale * 2))
-        v_dx_label.set_color(GREEN)
+        v_dx_label.set_color(dx_color)
         v_dy_label = TexText("$\Delta_y f$")
         v_dy_label.scale(frame_scale * text_scale)
         v_dy_label.next_to(dy_dot.get_center(), LEFT*(frame_scale * text_scale * 2))
-        v_dy_label.set_color(RED)
+        v_dy_label.set_color(dy_color)
 
         self.play(
             Write(v_dx),
@@ -314,18 +320,18 @@ class CR_Visualize_right_side(Scene):
         # analyze u, v
         v_u_dx_point = [dx_point[0], base_point[1], 0.]
         v_u_dy_point = [dy_point[0], base_point[1], 0.]
-        v_u_dx = Arrow(start=base_point, end=v_u_dx_point, buff=0, stroke_color=BLUE, stroke_width = arrow_stroke_width, max_tip_length_to_length_ratio=arrow_max_tip_length_to_length_ratio)
-        v_u_dy = Arrow(start=base_point, end=v_u_dy_point, buff=0, stroke_color=ORANGE, stroke_width = arrow_stroke_width, max_tip_length_to_length_ratio=arrow_max_tip_length_to_length_ratio)
+        v_u_dx = Arrow(start=base_point, end=v_u_dx_point, buff=0, stroke_color=u_v_dx_color, stroke_width = arrow_stroke_width, max_tip_length_to_length_ratio=arrow_max_tip_length_to_length_ratio)
+        v_u_dy = Arrow(start=base_point, end=v_u_dy_point, buff=0, stroke_color=u_v_dy_color, stroke_width = arrow_stroke_width, max_tip_length_to_length_ratio=arrow_max_tip_length_to_length_ratio)
         v_u_dx_label = TexText("$\Delta_x u$")
         v_u_dx_label.scale(frame_scale * text_scale)
         v_u_dx_label.next_to(v_u_dx_point, RIGHT*(frame_scale * text_scale))
-        v_u_dx_label.set_color(BLUE)
+        v_u_dx_label.set_color(u_v_dx_color)
         v_u_dy_label = TexText("$\Delta_y u$")
         v_u_dy_label.scale(frame_scale * text_scale)
         v_u_dy_label.next_to(v_u_dy_point, LEFT*(frame_scale * text_scale))
-        v_u_dy_label.set_color(ORANGE)
-        v_u_dx_line = DashedLine(start=v_u_dx_point, end=dx_point, stroke_color=BLUE, stroke_width = arrow_stroke_width/3)
-        v_u_dy_line = DashedLine(start=v_u_dy_point, end=dy_point, stroke_color=ORANGE, stroke_width = arrow_stroke_width/3)
+        v_u_dy_label.set_color(u_v_dy_color)
+        v_u_dx_line = DashedLine(start=v_u_dx_point, end=dx_point, stroke_color=u_v_dx_color, stroke_width = arrow_stroke_width/3)
+        v_u_dy_line = DashedLine(start=v_u_dy_point, end=dy_point, stroke_color=u_v_dy_color, stroke_width = arrow_stroke_width/3)
 
         self.play(
             Write(v_u_dx),
@@ -345,18 +351,18 @@ class CR_Visualize_right_side(Scene):
 
         v_v_dx_point = [base_point[0], dx_point[1], 0.]
         v_v_dy_point = [base_point[0], dy_point[1], 0.]
-        v_v_dx = Arrow(start=base_point, end=v_v_dx_point, buff=0, stroke_color=BLUE, stroke_width = arrow_stroke_width, max_tip_length_to_length_ratio=arrow_max_tip_length_to_length_ratio)
-        v_v_dy = Arrow(start=base_point, end=v_v_dy_point, buff=0, stroke_color=ORANGE, stroke_width = arrow_stroke_width, max_tip_length_to_length_ratio=arrow_max_tip_length_to_length_ratio)
+        v_v_dx = Arrow(start=base_point, end=v_v_dx_point, buff=0, stroke_color=u_v_dx_color, stroke_width = arrow_stroke_width, max_tip_length_to_length_ratio=arrow_max_tip_length_to_length_ratio)
+        v_v_dy = Arrow(start=base_point, end=v_v_dy_point, buff=0, stroke_color=u_v_dy_color, stroke_width = arrow_stroke_width, max_tip_length_to_length_ratio=arrow_max_tip_length_to_length_ratio)
         v_v_dx_label = TexText("$\Delta_x v$")
         v_v_dx_label.scale(frame_scale * text_scale)
         v_v_dx_label.next_to(v_v_dx_point, LEFT*(frame_scale * text_scale))
-        v_v_dx_label.set_color(BLUE)
+        v_v_dx_label.set_color(u_v_dx_color)
         v_v_dy_label = TexText("$\Delta_y v$")
         v_v_dy_label.scale(frame_scale * text_scale)
         v_v_dy_label.next_to(v_v_dy_point, LEFT*(frame_scale * text_scale))
-        v_v_dy_label.set_color(ORANGE)
-        v_v_dx_line = DashedLine(start=v_v_dx_point, end=dx_point, stroke_color=BLUE, stroke_width = arrow_stroke_width/3)
-        v_v_dy_line = DashedLine(start=v_v_dy_point, end=dy_point, stroke_color=ORANGE, stroke_width = arrow_stroke_width/3)
+        v_v_dy_label.set_color(u_v_dy_color)
+        v_v_dx_line = DashedLine(start=v_v_dx_point, end=dx_point, stroke_color=u_v_dx_color, stroke_width = arrow_stroke_width/3)
+        v_v_dy_line = DashedLine(start=v_v_dy_point, end=dy_point, stroke_color=u_v_dy_color, stroke_width = arrow_stroke_width/3)
 
         self.play(
             Write(v_v_dx),
@@ -375,3 +381,119 @@ class CR_Visualize_right_side(Scene):
         self.wait(interval_time)
 
         self.wait(interval_time)
+
+#########################################################################################
+
+class CR_Visualize_text(Scene):
+    def construct(self):
+        text_scale = 1
+
+        #self.wait(2*animation_time)
+        #self.wait(interval_time)
+
+        # base point
+        text_base_point = Text("在复平面上取一点")
+        text_base_point.scale(text_scale)
+        text_base_point.to_corner(UP)
+        self.play(
+            Write(text_base_point, run_time=0.3*animation_time),
+        )
+        self.wait(0.7*animation_time)
+        self.play(
+            FadeOut(text_base_point, run_time=interval_time),
+        )
+
+        ## sync point 1 - write coordinate
+        ## 3 t1 + 2 t2 used
+
+        # dx, dy
+        text_dx_dy_1 = Text("并设在它的邻域内有向实轴方向的微小移动 ")
+        text_dx = TexText("$\Delta x$ \\rightarrow 0", color=dx_color)
+        text_dx_dy_2 = Text(" 与向虚轴方向的微小移动 ")
+        text_dy = TexText("$\Delta y$ \\rightarrow 0", color=dy_color)
+        group_dx_dy = VGroup(VGroup(text_dx_dy_1, text_dx).arrange(), VGroup(text_dx_dy_2, text_dy).arrange())
+        group_dx_dy.arrange(DOWN, aligned_edge=RIGHT)
+        group_dx_dy.scale(text_scale)
+        group_dx_dy.to_corner(UP)
+
+        self.play(
+            Write(text_dx_dy_1, run_time=0.8*animation_time),
+        )
+        self.play(
+            Write(text_dx, run_time=0.2*animation_time),
+        )
+        self.wait(interval_time)
+
+        self.play(
+            Write(text_dx_dy_2, run_time=0.6*animation_time),
+        )
+        self.play(
+            Write(text_dy, run_time=0.2*animation_time),
+        )
+        self.wait(0.2*animation_time)
+        self.play(
+            FadeOut(text_dx_dy_1),
+            FadeOut(text_dx),
+            FadeOut(text_dx_dy_2),
+            FadeOut(text_dy),
+            runtime=interval_time
+        )
+
+        ## sync point 2 - dx and dy drawn
+        ## 2 t1 + 2 t2
+
+        text_transformation = TexText("考虑一个解析函数 $f(z)$，变换前后如上")
+        text_transformation.to_corner(UP)
+        self.play(
+            Write(text_transformation, run_time=1.5*animation_time),
+        )
+        self.wait(2.5*animation_time)
+        self.play(
+            FadeOut(text_transformation),
+            runtime=interval_time
+        )
+
+        ## sync point 3 - transformation applied
+        ## 4 t1 + t2
+
+        text_zoom = Text("考虑这一点的局域")
+        text_zoom.to_corner(UP)
+        self.play(
+            Write(text_zoom, run_time=animation_time),
+        )
+        self.wait(animation_time)
+        self.wait(interval_time)
+        self.play(
+            FadeOut(text_zoom),
+            runtime=interval_time
+        )
+
+        ## sync point 4 - camera shifted and zoomed
+        # 2 t1 + 2 t2
+
+        text_and_1 = TexText(" 和 ")
+        text_df_1 = TexText(" 两个位移分别引起 $f$ 的两种变化 ")
+        text_dx_f = TexText("$\Delta_x f$", color=dx_color)
+        text_and_2 = TexText(" 和 ")
+        text_dy_f = TexText("$\Delta_y f$", color=dy_color)
+        group_df = VGroup(text_dx, text_and_1, text_dy, text_df_1, text_dx_f, text_and_2, text_dy_f)
+        group_df.arrange(RIGHT)
+        group_df.to_corner(UP)
+        self.play(
+            Write(group_df, run_time=animation_time),
+        )
+        self.wait(interval_time)
+        self.wait(animation_time)
+        self.wait(interval_time)
+
+        ## sync point 5 - vectors and labels
+        # 2 t1 + 2 t2
+
+        self.wait(4*animation_time)
+        self.wait(interval_time)
+        self.play(
+            FadeOut(group_df),
+            runtime=interval_time
+        )
+
+        text_proof = TexText("由复变函数导数的几何性质，")

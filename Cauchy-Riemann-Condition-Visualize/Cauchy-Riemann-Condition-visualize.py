512236fadac41642a84dc7c57c3960c81bd5063c
fxzjshm <fxzjshm@163.com>
2021-11-21 00:29:05 +0800

    [CR-cond-visualize] draft version 1

--- Cauchy-Riemann-Condition-Visualize/Cauchy-Riemann-Condition-visualize.py
+++ Cauchy-Riemann-Condition-Visualize/Cauchy-Riemann-Condition-visualize.py
@@ -20,6 +20,10 @@ dy_color = RED
 u_v_dx_color = BLUE
 u_v_dy_color = ORANGE
 
+# for debugging sync
+sync_point = Text("Syncing ......")
+debug_sync = False
+
 class CR_Visualize_left_side(Scene):
     def construct(self):
         frame_scale = 0.6
@@ -48,6 +52,7 @@ class CR_Visualize_left_side(Scene):
         ## sync point 1 - write coordinate
         ## 3 t1 + 2 t2 used
         ## (t1 = animation_time, t2 = interval_time)
+        debug_sync_dot(self)
 
         # dx, dy
         dx_point = c_grid.coords_to_point(base_point_x + dx, base_point_y)
@@ -57,18 +62,22 @@ class CR_Visualize_left_side(Scene):
         
         self.play(
             Write(dx_dot),
-            run_time=animation_time
+            run_time=0.6*animation_time
         )
+        self.wait(0.2*animation_time)
         self.wait(interval_time)
 
         self.play(
             Write(dy_dot),
-            run_time=animation_time
+            run_time=0.6*animation_time
         )
+        self.wait(0.2*animation_time)
+        self.wait(0.4*animation_time)
         self.wait(interval_time)
 
         ## sync point 2 - dx and dy drawn
         ## 2 t1 + 2 t2
+        debug_sync_dot(self)
 
         # the other scene is being transformed, so just wait
         self.wait(4*animation_time)
@@ -76,6 +85,7 @@ class CR_Visualize_left_side(Scene):
 
         ## sync point 3 - transformation applied
         ## 4 t1 + t2
+        debug_sync_dot(self)
 
         # shift the camera and zoom in
         frame = self.camera.frame
@@ -97,6 +107,7 @@ class CR_Visualize_left_side(Scene):
 
         ## sync point 4 - camera shifted and zoomed
         # 2 t1 + 2 t2
+        debug_sync_dot(self)
 
         # arrows and labels
         base_point = base_dot.get_center()
@@ -129,6 +140,7 @@ class CR_Visualize_left_side(Scene):
 
         ## sync point 5 - vectors and labels
         # 2 t1 + 2 t2
+        debug_sync_dot(self)
 
         # moving dot
         moving_dot = Dot(base_point, color=YELLOW)
@@ -162,8 +174,20 @@ class CR_Visualize_left_side(Scene):
         )
         self.wait(interval_time)
 
-        ## sync point 6 - moving point
+        ## sync point 6 - moving dot
         # 4 t1 + 2 t2
+
+        self.wait(4*animation_time + 4*interval_time)
+        
+        ## sync point 7 - analyze
+        # 4 t1 + 4 t2
+        debug_sync_dot(self)
+
+        self.wait(3*animation_time + 10*interval_time)
+
+        ## sync point 8 - explanation
+        # 3 t1 + 10 t2
+        debug_sync_dot(self)
 
 #########################################################################################
 
@@ -194,6 +218,7 @@ class CR_Visualize_right_side(Scene):
 
         ## sync point 1 - write coordinate
         ## 3 t1 + 2 t2 used
+        debug_sync_dot(self)
 
         # dx, dy
         dx_point = moving_c_grid.coords_to_point(base_point_x + dx, base_point_y)
@@ -203,18 +228,22 @@ class CR_Visualize_right_side(Scene):
 
         self.play(
             Write(dx_dot),
-            run_time=animation_time
+            run_time=0.6*animation_time
         )
+        self.wait(0.2*animation_time)
         self.wait(interval_time)
 
         self.play(
             Write(dy_dot),
-            run_time=animation_time
+            run_time=0.6*animation_time
         )
+        self.wait(0.2*animation_time)
+        self.wait(0.4*animation_time)
         self.wait(interval_time)
 
         ## sync point 2 - dx and dy drawn
         ## 2 t1 + 2 t2
+        debug_sync_dot(self)
 
         # group altogether to apply transformation
         base_elements = VGroup(moving_c_grid, base_dot, dx_dot, dy_dot)
@@ -228,6 +257,7 @@ class CR_Visualize_right_side(Scene):
 
         ## sync point 3 - transformation applied
         # 4 t1 + t2
+        debug_sync_dot(self)
 
         # shift the camera and zoom in
         base_point = base_dot.get_center()
@@ -249,6 +279,7 @@ class CR_Visualize_right_side(Scene):
 
         ## sync point 4 - camera shifted and zoomed
         # 2 t1 + 2 t2
+        debug_sync_dot(self)
 
         # arrows and labels
         base_point = base_dot.get_center()
@@ -281,6 +312,7 @@ class CR_Visualize_right_side(Scene):
 
         ## sync point 5 - vectors and labels
         # 2 t1 + 2 t2
+        debug_sync_dot(self)
 
         # moving dot
         moving_dot = Dot(base_point, color=YELLOW)
@@ -314,8 +346,9 @@ class CR_Visualize_right_side(Scene):
         )
         self.wait(interval_time)
 
-        ## sync point 6 - moving point
+        ## sync point 6 - moving dot
         # 4 t1 + 2 t2
+        debug_sync_dot(self)
 
         # analyze u, v
         v_u_dx_point = [dx_point[0], base_point[1], 0.]
@@ -380,7 +413,15 @@ class CR_Visualize_right_side(Scene):
         )
         self.wait(interval_time)
 
-        self.wait(interval_time)
+        ## sync point 7 - analyze
+        # 4 t1 + 4 t2
+        debug_sync_dot(self)
+
+        self.wait(3*animation_time + 10*interval_time)
+
+        ## sync point 8 - explanation
+        # 3 t1 + 10 t2
+        debug_sync_dot(self)
 
 #########################################################################################
 
@@ -388,8 +429,8 @@ class CR_Visualize_text(Scene):
     def construct(self):
         text_scale = 1
 
-        #self.wait(2*animation_time)
-        #self.wait(interval_time)
+        self.wait(2*animation_time)
+        self.wait(interval_time)
 
         # base point
         text_base_point = Text("在复平面上取一点")
@@ -405,42 +446,45 @@ class CR_Visualize_text(Scene):
 
         ## sync point 1 - write coordinate
         ## 3 t1 + 2 t2 used
+        debug_sync_dot(self)
 
         # dx, dy
         text_dx_dy_1 = Text("并设在它的邻域内有向实轴方向的微小移动 ")
-        text_dx = TexText("$\Delta x$ \\rightarrow 0", color=dx_color)
+        text_dx = TexText("$\Delta x (\\rightarrow 0)$", color=dx_color)
         text_dx_dy_2 = Text(" 与向虚轴方向的微小移动 ")
-        text_dy = TexText("$\Delta y$ \\rightarrow 0", color=dy_color)
+        text_dy = TexText("$\Delta y (\\rightarrow 0)$", color=dy_color)
         group_dx_dy = VGroup(VGroup(text_dx_dy_1, text_dx).arrange(), VGroup(text_dx_dy_2, text_dy).arrange())
         group_dx_dy.arrange(DOWN, aligned_edge=RIGHT)
         group_dx_dy.scale(text_scale)
         group_dx_dy.to_corner(UP)
 
+        # for unknown reason here we need to be faster
         self.play(
-            Write(text_dx_dy_1, run_time=0.8*animation_time),
+            Write(text_dx_dy_1, run_time=0.4*animation_time),
         )
         self.play(
             Write(text_dx, run_time=0.2*animation_time),
         )
-        self.wait(interval_time)
-
+        
         self.play(
-            Write(text_dx_dy_2, run_time=0.6*animation_time),
+            Write(text_dx_dy_2, run_time=0.4*animation_time),
         )
         self.play(
             Write(text_dy, run_time=0.2*animation_time),
         )
-        self.wait(0.2*animation_time)
+        self.wait(0.4*animation_time)
+        self.wait(interval_time)
         self.play(
             FadeOut(text_dx_dy_1),
             FadeOut(text_dx),
             FadeOut(text_dx_dy_2),
             FadeOut(text_dy),
-            runtime=interval_time
+            runtime=0.8*interval_time
         )
 
         ## sync point 2 - dx and dy drawn
         ## 2 t1 + 2 t2
+        debug_sync_dot(self)
 
         text_transformation = TexText("考虑一个解析函数 $f(z)$，变换前后如上")
         text_transformation.to_corner(UP)
@@ -455,8 +499,9 @@ class CR_Visualize_text(Scene):
 
         ## sync point 3 - transformation applied
         ## 4 t1 + t2
+        debug_sync_dot(self)
 
-        text_zoom = Text("考虑这一点的局域")
+        text_zoom = Text("在这一点的邻域")
         text_zoom.to_corner(UP)
         self.play(
             Write(text_zoom, run_time=animation_time),
@@ -470,6 +515,7 @@ class CR_Visualize_text(Scene):
 
         ## sync point 4 - camera shifted and zoomed
         # 2 t1 + 2 t2
+        debug_sync_dot(self)
 
         text_and_1 = TexText(" 和 ")
         text_df_1 = TexText(" 两个位移分别引起 $f$ 的两种变化 ")
@@ -478,6 +524,7 @@ class CR_Visualize_text(Scene):
         text_dy_f = TexText("$\Delta_y f$", color=dy_color)
         group_df = VGroup(text_dx, text_and_1, text_dy, text_df_1, text_dx_f, text_and_2, text_dy_f)
         group_df.arrange(RIGHT)
+        group_df.scale(0.8)
         group_df.to_corner(UP)
         self.play(
             Write(group_df, run_time=animation_time),
@@ -488,6 +535,7 @@ class CR_Visualize_text(Scene):
 
         ## sync point 5 - vectors and labels
         # 2 t1 + 2 t2
+        debug_sync_dot(self)
 
         self.wait(4*animation_time)
         self.wait(interval_time)
@@ -496,4 +544,103 @@ class CR_Visualize_text(Scene):
             runtime=interval_time
         )
 
-        text_proof = TexText("由复变函数导数的几何性质，")
+        ## sync point 6 - moving point
+        # 4 t1 + 2 t2
+        debug_sync_dot(self)
+
+        text_analyze = TexText("分解如上")
+        group_analyze = VGroup(text_dx_f, text_and_2, text_dy_f, text_analyze)
+        group_analyze.arrange(RIGHT)
+        group_analyze.to_corner(UP)
+        self.play(
+            Write(group_analyze, run_time=animation_time),
+        )
+        self.wait(2*interval_time)
+        self.play(
+            FadeOut(group_analyze),
+            runtime=interval_time
+        )
+        # speed up a bit here
+        self.wait(3*animation_time + 0*interval_time)
+        
+        ## sync point 7 - analyze
+        # 4 t1 + 4 t2
+        debug_sync_dot(self)
+
+        # explanation
+        text_proof_1 = Text("由复变函数导数的几何性质，")
+        text_proof_2 = Text(" 的模长应当相同，")
+        text_proof_3 = Text("夹角与 ")
+        text_proof_4 = Text(" 的夹角相同，即逆时针转过 90°")
+        group_proof_1 = VGroup(text_proof_1, text_dx_f, text_and_1, text_dy_f, text_proof_2).arrange(RIGHT)
+        group_proof_2 = VGroup(text_proof_3, text_dx, text_and_2, text_dy, text_proof_4).arrange(RIGHT)
+        group_proof = VGroup(group_proof_1, group_proof_2).arrange(DOWN)
+
+        text_CR_1 = Tex("\\therefore", "\Delta_x u", "=", "\Delta_y v", ", ", "\Delta_y u", "=", "-", "\Delta_x v")
+        text_CR_1.set_color_by_tex_to_color_map({
+            "\Delta_x u": u_v_dx_color,
+            "\Delta_y u": u_v_dy_color,
+            "\Delta_x v": u_v_dx_color,
+            "\Delta_y v": u_v_dy_color
+        })
+
+        text_CR_2 = Tex("\\therefore", "\partial_x u", "=", "\partial_y v", ", ", "\partial_y u", "=", "-", "\partial_x v")
+        text_CR_2.set_color_by_tex_to_color_map({
+            "\partial_x u": u_v_dx_color,
+            "\partial_y u": u_v_dy_color,
+            "\partial_x v": u_v_dx_color,
+            "\partial_y v": u_v_dy_color
+        })
+
+        group_final = VGroup(group_proof)
+        group_final.arrange(DOWN)
+        group_final.to_corner(UP)
+
+        text_CR_1.to_corner(UP)
+        text_CR_2.to_corner(UP)
+
+        self.play(
+            Write(group_proof, run_time=animation_time),
+        )
+        self.wait(2*interval_time)
+
+        self.play(
+            FadeOut(group_proof, run_time=animation_time),
+        )
+        self.wait(interval_time)
+
+        self.play(
+            Write(text_CR_1, run_time=animation_time),
+        )
+        self.wait(2*interval_time)
+
+
+        self.play(
+            TransformMatchingTex(
+                text_CR_1, text_CR_2,
+                key_map={
+                    "\Delta_x u": "\partial_x u",
+                    "\Delta_y u": "\partial_y u",
+                    "\Delta_x v": "\partial_x v",
+                    "\Delta_y v": "\partial_y v"
+                }
+            ),
+            run_time=animation_time
+        )
+        self.wait(5*interval_time)
+
+        ## sync point 8 - explanation
+        # 3 t1 + 10 t2
+        debug_sync_dot(self)
+
+def debug_sync_dot(scene):
+    if debug_sync:
+        sync_point.arrange()
+        scene.play(
+            Write(sync_point),
+            run_time=animation_time
+        )
+        scene.play(
+            FadeOut(sync_point),
+            run_time=interval_time
+        )
